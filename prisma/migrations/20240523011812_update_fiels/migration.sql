98218710a53fb88d11d710a5a44075690f9d3ac2
Larissa Santin <lari_santin@hotmail.com>
2024-05-24 17:21:52 -0300

    Delete prisma/migrations directory

--- prisma/migrations/20240523011812_update_fiels/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `quantity` on the `books` table. The data in that column could be lost. The data in that column will be cast from `BigInt` to `Integer`.
-
-*/
--- AlterTable
-ALTER TABLE "books" ALTER COLUMN "price" SET DATA TYPE DOUBLE PRECISION,
-ALTER COLUMN "quantity" SET DATA TYPE INTEGER;
